74e83f62cede7413fd8da291ec3f93c00df9ae9a
Kristina Tymofiichuk <99436996+tymokri@users.noreply.github.com>
2022-12-19 19:40:51 +0000

    Initial commit

---
+++ my-app/README.md
@@ -0,0 +1,5 @@
+# JSR-hw-1
+
+1. Install React
+2. Make component Card
+3. Import card to App.js and render it
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Card from './components/Card'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
